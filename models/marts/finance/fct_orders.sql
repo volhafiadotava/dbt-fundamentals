69e0ab5bca61e9633dde4ab012f8ae807bd6cd49
Volha Fiadotava <volha.fiadotava@theinformationlab.de>
2025-08-04 16:10:34 +0000

    integrate Stripe payments data into fct_orders model

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,27 +1,33 @@
-with orders as  (
-    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+with orders as (
+
+select*
+from {{ ref('stg_jaffle_shop__orders')}}
+
 ),
 
 payments as (
-    select * from {{ ref ('stg_stripe__payments') }}
+
+select*
+from {{ ref('stg_stripe__payments') }}
 ),
 
 order_payments as (
     select
         order_id,
-        sum (case when status = 'success' then amount end) as amount
-
+        sum(case when status ='success' then amount end) as amount
+    
     from payments
-    group by 1
+    group by order_id
+
 ),
 
- final as (
 
+final as (
     select
         orders.order_id,
         orders.customer_id,
         orders.order_date,
-        coalesce (order_payments.amount, 0) as amount
+        coalesce(order_payments.amount, 0) as amount
 
     from orders
     left join order_payments using (order_id)
---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,8 @@
+SELECT 
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100 as amount,
+    created as created_at
+FROM raw.stripe.payment
